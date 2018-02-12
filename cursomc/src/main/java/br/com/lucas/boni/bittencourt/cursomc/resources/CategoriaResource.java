050187eefcf2c2380916928f391622997dc145ac
Lucas <lucasbonibittencourt@gmail.com>
2018-02-12 09:09:51 -0200

    inserindo uma nova CATEGORIA com post

--- cursomc/src/main/java/br/com/lucas/boni/bittencourt/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/br/com/lucas/boni/bittencourt/cursomc/resources/CategoriaResource.java
@@ -4,10 +4,10 @@ import br.com.lucas.boni.bittencourt.cursomc.domain.Categoria;
 import br.com.lucas.boni.bittencourt.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -21,6 +21,15 @@ public class CategoriaResource {
 
         Categoria buscar = service.buscar(id);
         return ResponseEntity.ok().body(buscar);
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {      //@RequestBody faz o json ser convertido para o objeto
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();//cria a urido novo recurso que foi criado
+        return ResponseEntity.created(uri).build();
+
     }
 
 }
