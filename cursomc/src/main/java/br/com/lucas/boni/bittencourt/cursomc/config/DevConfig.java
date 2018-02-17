fc7877a6d3309beaae229d81be65cbecf5e57f25
Lucas <lucasbonibittencourt@gmail.com>
2018-02-17 12:26:53 -0200

    -CRIADOO PROFILE DEV

--- cursomc/src/main/java/br/com/lucas/boni/bittencourt/cursomc/config/DevConfig.java
+++ cursomc/src/main/java/br/com/lucas/boni/bittencourt/cursomc/config/DevConfig.java
@@ -2,6 +2,7 @@ package br.com.lucas.boni.bittencourt.cursomc.config;
 
 import br.com.lucas.boni.bittencourt.cursomc.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,15 +14,22 @@ import java.text.ParseException;
  * COLOCA AS CLASSES QUE SERAO INSTANCIADAS NO PROFILE ESCOLHHIDO
  */
 @Configuration
-@Profile(value = "test")
-public class TestConfig {
+@Profile(value = "dev")
+public class DevConfig {
 
     @Autowired
     private DBService service;
 
-    @Bean
+             @Value("${spring.jpa.hibernate.ddl-auto}")//pega os dados de uma propriedade
+             private String stratagy;
+
+    @Bean//é obrigatorio retornar alguma coisa por isso retorna um true sempre
     public boolean instanciateDataBase() throws ParseException {
-        service.instanciedTestDataBase();       //popula o banco de teste
-        return true;
+        if(!stratagy.equalsIgnoreCase("create")){
+            return false;
+        }else {
+            service.instanciedTestDataBase();       //popula o banco de teste
+            return true;
+        }
     }
 }
