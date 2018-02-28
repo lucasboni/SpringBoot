c3e86432fb74bc133b6bdbacc3d0592fffae15eb
lucas <lucas@zaruc.com.br>
2018-02-28 13:48:58 -0300

    -inicio da implementacao da autenticacao do usuario

--- src/main/java/br/com/lucas/boni/bittencourt/cursomc/config/SecurityConfig.java
+++ src/main/java/br/com/lucas/boni/bittencourt/cursomc/config/SecurityConfig.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -22,6 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment env;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     public static final String[] PUBLIC_MATCHERS = {
            "/h2-console/**",
@@ -47,6 +52,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//assegura que noa cria sessao
     }
 
+
+    /**
+     * NESCESSARIO PARA A AUTENTICACAO DO USUARIO
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
 
     /**
      * Copiei de https://auth0.com/blog/implementing-jwt-authentication-on-spring-boot/
