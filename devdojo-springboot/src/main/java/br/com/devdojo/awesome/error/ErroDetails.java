fe915d7b1ecba4c2cdc61e0cafc091e2201e145a
Lucas <lucasbonibittencourt@gmail.com>
2020-12-13 07:37:33 -0300

    -Spring Boot Essentials 14 - Tratamento de erros em REST pt 05 - Padronizando todos os erros -https://www.youtube.com/watch?v=Hl-5KcmRiDQ&list=PL62G310vn6nF3gssjqfCKLpTK2sZJ_a_1&index=15&ab_channel=DevDojo

--- devdojo-springboot/src/main/java/br/com/devdojo/awesome/error/ErroDetails.java
+++ devdojo-springboot/src/main/java/br/com/devdojo/awesome/error/ErroDetails.java
@@ -12,8 +12,6 @@ public class ErroDetails {
         return title;
     }
 
-
-
     public int getStatus() {
         return status;
     }
@@ -37,5 +35,56 @@ public class ErroDetails {
 
     public void setDeveloperMessage(String developerMessage) {
         this.developerMessage = developerMessage;
+    }
+
+
+    public static final class Builder {
+        protected String title;
+        protected int status;
+        protected String detail;
+        protected long timestamp;
+        protected String developerMessage;
+
+        private Builder() {
+        }
+
+        public static Builder newBuilder() {
+            return new Builder();
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder status(int status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder detail(String detail) {
+            this.detail = detail;
+            return this;
+        }
+
+        public Builder timestamp(long timestamp) {
+            this.timestamp = timestamp;
+            return this;
+        }
+
+        public Builder developerMessage(String developerMessage) {
+            this.developerMessage = developerMessage;
+            return this;
+        }
+
+        public ErroDetails build() {
+            ErroDetails erroDetails = new ErroDetails();
+            erroDetails.setTimestamp(timestamp);
+            erroDetails.setDeveloperMessage(developerMessage);
+            erroDetails.status = this.status;
+            erroDetails.title = this.title;
+            erroDetails.detail = this.detail;
+            return erroDetails;
+        }
     }
 }
--- devdojo-springboot/src/main/java/br/com/devdojo/awesome/handler/RestExeptionHandler.java
+++ devdojo-springboot/src/main/java/br/com/devdojo/awesome/handler/RestExeptionHandler.java
@@ -1,14 +1,19 @@
 package br.com.devdojo.awesome.handler;
 
+import br.com.devdojo.awesome.error.ErroDetails;
 import br.com.devdojo.awesome.error.ResorceNotFoundDetails;
 import br.com.devdojo.awesome.error.ResorceNotFoundException;
 import br.com.devdojo.awesome.error.ValidationErroDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 import java.util.List;
@@ -18,7 +23,21 @@ import java.util.stream.Collectors;
  * Esta classe pega o evento, de erro e monta o json da maneitra desejada
  */
 @ControllerAdvice
-public class RestExeptionHandler {
+public class RestExeptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @Override  //seria a chamada generica, caso nao tenh tratamento especifico usa este
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErroDetails erroDetails = ErroDetails.Builder
+                .newBuilder()
+                .timestamp(new Date().getTime())
+                .status(HttpStatus.NOT_FOUND.value())
+                .title("Internal Exeption")
+                .detail(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+        return new ResponseEntity<>(erroDetails,headers,status);
+    }
 
     @ExceptionHandler(ResorceNotFoundException.class)
     public ResponseEntity<?> handlerResourceNotFoundExeption(ResorceNotFoundException rnfException){
@@ -33,7 +52,7 @@ public class RestExeptionHandler {
         return new ResponseEntity<>(resorceNotFound,HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    /*@ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handlermethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException){
         List<FieldError> fieldErrors = methodArgumentNotValidException.getBindingResult().getFieldErrors();
         ValidationErroDetails resorceNotFound = ValidationErroDetails.Builder
@@ -47,5 +66,40 @@ public class RestExeptionHandler {
                 .fieldMessage(fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(",")))
                 .build();
         return new ResponseEntity<>(resorceNotFound,HttpStatus.BAD_REQUEST);
+    }*/
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException methodArgumentNotValidException, HttpHeaders headers, HttpStatus status, WebRequest request){
+        List<FieldError> fieldErrors = methodArgumentNotValidException.getBindingResult().getFieldErrors();
+        ValidationErroDetails resorceNotFound = ValidationErroDetails.Builder
+                .newBuilder()
+                .timestamp(new Date().getTime())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .title("Fild Validation Error")
+                .detail("Fild Validation Error")
+                .developerMessage(methodArgumentNotValidException.getClass().getName())
+                .field(fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(",")))
+                .fieldMessage(fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(",")))
+                .build();
+        return new ResponseEntity<>(resorceNotFound,HttpStatus.BAD_REQUEST);
+    }
+
+    //@Override
+    //protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    //    return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    //}
+
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErroDetails erroDetails = ErroDetails.Builder
+                .newBuilder()
+                .timestamp(new Date().getTime())
+                .status(HttpStatus.NOT_FOUND.value())
+                .title("Resorce not found")
+                .detail(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+        return new ResponseEntity<>(erroDetails,HttpStatus.BAD_REQUEST);
     }
 }
